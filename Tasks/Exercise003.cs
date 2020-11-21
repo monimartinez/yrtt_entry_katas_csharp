45fb7e32899b9269a50a2a69dc58e6a7b0440683
monimartinez <74716035+monimartinez@users.noreply.github.com>
2020-11-21 09:41:55 +0000

    Update Exercise003.cs
    Updated code for revision Exercise003.cs as  as I ran it like this on my Desktop .Net. Please see files submitted in Google Classroom yesterday.
    Worked as much as I could on calculations, printed three different values separately (commented and ran each value apart) as I did not manage to send them in one go, please see screenshot in .Net.

--- Tasks/Exercise003.cs
+++ Tasks/Exercise003.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 // Scenario
 // Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
@@ -23,14 +24,90 @@ using System;
 // Explanation:
 // The first element 80 is the total weight of team 1, and the second element 0 is the total weight of team 2.
 
-namespace TechReturners.Tasks
-{
-    public class Exercise003
-    {
-        public static int[] RowWeights(int[] a)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
 
+//Changed namespace to "Test" instead of TechReturners.Tasks (created another project) - as I ran it like this on my Desktop .Net (please see files submitted in Google Classroom with screenshot)
+namespace Test
+{
+     class Program
+    {
+    public static int MinimumCost(int []cost, int n, int W) 
+    { 
+          
+        // val[] and wt[] arrays 
+        // val[] array to 'weight' of Team members
+        List<int> val = new List<int>(); 
+        List<int> wt = new List<int>(); 
+  
+  	// Traverse the wt[] array and skip 
+        // unavailable weights and make val[] and wt[] 
+        // array. Size variable tells the available 
+        // number of distinct weights (values)
+        // for each Team member. 
+
+        int size = 0; 
+        for (int i = 0; i < n; i++) 
+        { 
+            if (cost[i] != -1) 
+            { 
+                val.Add(cost[i]); 
+                wt.Add(i + 1); 
+                size++; 
+            } 
+        } 
+     
+        n = size; 
+        int [,]min_cost = new int[n+1,W+1];
+        
+        // Fill 0th row with infinity 
+        for (int i = 0; i <= W; i++) 
+           min_cost[0,i] = int.MaxValue; 
+        
+	// Fill 0th column with 0 
+        for (int i = 1; i <= n; i++) 
+            min_cost[i,0] = 0;  
+  
+	// Now check for each weight one by one and 
+        // fill the matrix according to the condition 
+        for (int i = 1; i <= n; i++) 
+        { 
+            for (int j = 1; j <= W; j++) 
+            { 
+                // wt[i-1]>j means capacity of bag is 
+                // less than weight of item 
+                if (wt[i-1] > j) 
+                    min_cost[i,j] = min_cost[i-1,j]; 
+     
+                // here we check we get minimum value of weight 
+                // either by including it or excluding 
+                // it 
+                else
+                    min_cost[i,j] = Math.Min(min_cost[i-1,j], 
+                                min_cost[i,j-wt[i-1]] + val[i-1]); 
+  		} 
+       	 } 
+     
+        // Exactly weight W can not be made by 
+        // given weights 
+        return (min_cost[n,W] == int.MaxValue )? -1: min_cost[n,W]; 
+    } 
+  
+    // Driver Code 
+    public static void Main() 
+    { 
+//      int []cost = {13, 27, 49};
+//          int W = 3; 
+//          int n = cost.Length; 
+//          Console.WriteLine("Return (62," + MinimumCost(cost, n, W) + ")"); 
+
+//        int []cost = {50, 60, 70, 80};
+//            int W = 4; 
+//            int n = cost.Length; 
+//	    Console.WriteLine("Return (120," + MinimumCost(cost, n, W) + ")"); 
+	
+	int []cost = {0};
+           int W = 1; 
+           int n = cost.Length; 
+	   Console.WriteLine("Return (80," + MinimumCost(cost, n, W) + ")"); 
+      } 
+  } 
+}
